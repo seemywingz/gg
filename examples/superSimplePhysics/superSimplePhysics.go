0c2edaaf8ec9ab8538dfe0f2731eed6ba43c1d66
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-12-03 15:56:32 -0500

    2d box physics working

--- examples/superSimplePhysics/superSimplePhysics.go
+++ examples/superSimplePhysics/superSimplePhysics.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/seemywingz/in3D"
 )
 
@@ -9,8 +10,9 @@ var objects []*in3D.DrawnObject
 func main() {
 
 	in3D.Init(800, 600, "Simple Cube in3D")
+	window := in3D.GetWindow()
 	light := in3D.NewLight()
-	light.Position = in3D.NewPosition(10, 1, 10)
+	light.Position = in3D.NewPosition(0, 1, 10)
 	light.Radius = 1000
 
 	in3D.GetCamera().Position = in3D.NewPosition(0, 80, 400)
@@ -19,9 +21,25 @@ func main() {
 	in3D.Enable(in3D.PointerLock, true)
 	in3D.Enable(in3D.FlyMode, true)
 
-	in3D.SetRelPath("../assets/textures")
-	// texture := in3D.NewTexture("seemywingz.jpg")
+	explode()
 
+	for !in3D.ShouldClose() {
+		in3D.Update()
+
+		// Press w
+		if window.GetKey(glfw.KeyF) == glfw.Press {
+			explode()
+		}
+
+		for _, obj := range objects {
+			obj.Draw()
+		}
+
+		in3D.SwapBuffers()
+	}
+}
+
+func explode() {
 	for i := 1; i < 1000; i++ {
 		obj := in3D.NewPointsObject(
 			in3D.NewPosition(0, float32(i), -20),
@@ -31,15 +49,5 @@ func main() {
 			in3D.Shader["phong"],
 		)
 		objects = append(objects, obj)
-	}
-
-	for !in3D.ShouldClose() {
-		in3D.Update()
-
-		for _, obj := range objects {
-			obj.Draw()
-		}
-
-		in3D.SwapBuffers()
 	}
 }
